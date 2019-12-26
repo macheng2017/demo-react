7ce5cb3127fa61baf2e32f0b0bfb35a0710ff7a4
macheng2017 <gos360@163.com>
2019-12-26 22:26:57 +0800

    add:在return函数中清除effect

--- src/components/clickTracker.js
+++ src/components/clickTracker.js
@@ -3,11 +3,19 @@ import React, {useState, useEffect} from "react";
 const MouseTracker = () => {
     const [position, setPosition] = useState({x: 0, y: 0})
     useEffect(() => {
-        document.addEventListener('click', event => {
+        const updatePosition = event => {
+            // 为什么会出现这种情况?
             // 这里有个严重的错误,点击几次后会几何级的创建click的监听,导致浏览器卡死
             console.log("inner")
             setPosition({x: event.clientX, y: event.clientY})
-        })
+        }
+        console.log("add listener")
+        document.addEventListener('click', updatePosition)
+        // 清除组件的effect
+        return ()=>{
+            console.log('remove listener')
+            document.removeEventListener('click',updatePosition)
+        }
     })
     return (<p>
         x:{position.x}
