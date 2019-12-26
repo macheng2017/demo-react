a9bd0ccb28531fa0849ceb54059e4c124a7cc08b
cheng ma <macheng2025@gmail.com>
2019-12-26 18:39:28 +0800

    add:useEffect每次渲染之后执行,类似go语言中的defer

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 // 定义一个组件
 const LikeButton = () => {
     // 第一个参数是状态名称,第二个参数是设置第一个状态的方法名以setXXX开始
     const [like, setLike] = useState(0)
     const [on, setOn] = useState(true)
-    const [count,setCount] = useState(0)
-
+    const [count, setCount] = useState(0)
+    // 每次渲染之后执行,类似go语言中的defer
+    useEffect(() => {
+        document.title = `点击了${like}次`
+    })
     // 组件的view部分
     return (
         <div>
@@ -19,8 +22,8 @@ const LikeButton = () => {
             }}>
                 {on ? 'On' : 'Off'}
             </button>
-            <button onClick={()=>{
-                setCount(count+1)
+            <button onClick={() => {
+                setCount(count + 1)
             }}>
                 {count}
             </button>
